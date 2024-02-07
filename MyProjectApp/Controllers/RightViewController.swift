0bb4a766ebc7e0fdb1594588eea2207b42be293d
DmytroNif <dimacool888@gmail.com>
2024-02-07 13:09:28 +0200

    rightView TableView

--- MyProjectApp/Controllers/RightViewController.swift
+++ MyProjectApp/Controllers/RightViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class RightViewController: UIViewController {
 
+    let mainView = RightView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    override func loadView() {
+        super.loadView()
+        view = mainView
+    }
+    //MARK: - UITableViewDataSource methods
 
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // Повернення кількості рядків у таблиці
+            return 10
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // Створення та налаштування рядка
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = "Row \(indexPath.row)"
+            return cell
+        }
+
+        //MARK: - UITableViewDelegate methods
+
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Обробка події вибору рядка
+            print("Selected row \(indexPath.row)")
+        }
 }
--- MyProjectApp/Views/RightView.swift
+++ MyProjectApp/Views/RightView.swift
@@ -7,9 +7,37 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class RightView: UIView{
     
+    let containerView: UIView = {
+        let obj = UIView()
+        obj.backgroundColor = .white
+        return obj
+    }()
+    
+    let tableView: UITableView = {
+        
+       return obj
+    }()
+    
+    private func addSubviews(){
+        self.addSubview(containerView)
+        containerView.addSubview(search)
+        containerView.addSubview(tableView)
+    }
+    
+    private func makeConstraints(){
+        
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(containerView)
+        }
+    }
 }
 
 
