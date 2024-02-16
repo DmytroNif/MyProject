a7365f0e7fbdcf2dd6b487fb832458a1a7772532
VolberdWork <vova.kolomyltsev@gmail.com>
2024-02-16 19:02:59 +0200

    rightView

--- Controllers/RightViewController.swift
+++ Controllers/RightViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class RightViewController: UIViewController {
+class RightViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let mainView = RightView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
+        mainView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    override func loadView() {
+        super.loadView()
+        view = mainView
+    }
+    //MARK: - UITableViewDataSource methods
 
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // Повернення кількості рядків у таблиці
+            return 10
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // Створення та налаштування рядка
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = "Row \(indexPath.row)"
+            return cell
+        }
+
+        //MARK: - UITableViewDelegate methods
+
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Обробка події вибору рядка
+            print("Selected row \(indexPath.row)")
+        }
 }
--- Views/RightView.swift
+++ Views/RightView.swift
@@ -7,9 +7,52 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class RightView: UIView{
     
+    let containerView: UIView = {
+        let obj = UIView()
+        obj.backgroundColor = .white
+        return obj
+    }()
+    
+    let tableView: UITableView = {
+        let obj = UITableView()
+       return obj
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        RightSetup()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews(){
+        self.addSubview(containerView)
+//        containerView.addSubview(search)
+        containerView.addSubview(tableView)
+    }
+    
+    private func makeConstraints(){
+        
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(containerView)
+        }
+    }
+    private func RightSetup() {
+        addSubviews()
+        makeConstraints()
+    }
 }
 
 
