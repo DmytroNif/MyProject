58be32698bfc5da1c8e7fddc038f74db4a9ff93c
DmytroNif <dimacool888@gmail.com>
2024-02-18 21:28:09 +0200

    UISearchBar with Textfield

--- MyProjectApp/Views/LeftView/LeftView.swift
+++ MyProjectApp/Views/LeftView/LeftView.swift
@@ -17,21 +17,41 @@ class LeftView: UIView{
         return obj
     }()
     
-    let search: UISearchBar = {
-        let obj = UISearchBar()
-        obj.backgroundColor = .systemBlue
-        obj.barTintColor = .lightGray.withAlphaComponent(0.5)
-        obj.isHidden = true
-        obj.layer.cornerRadius = 10
-        return obj
-    }()
+    let searchTextField: UITextField = {
+            let textField = UITextField()
+            textField.backgroundColor = UIColor.lightGray
+            textField.layer.cornerRadius = 5
+            textField.clipsToBounds = true
+        
+        // Setting up leftView with an image
+                let imageView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+                imageView.contentMode = .scaleAspectFit
+                let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 20)) // Adjust the frame as per your needs
+                imageView.frame = paddingView.bounds
+                paddingView.addSubview(imageView)
+                textField.leftView = paddingView
+                textField.leftViewMode = .always
+                
+                textField.placeholder = "Search..."
+                
+                return textField
+        }()
     
-    let showSearch: UIButton = {
-        let obj = UIButton()
-        obj.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
-        obj.isHidden = false
-        return obj
-    }()
+//    let search: UISearchBar = {
+//        let obj = UISearchBar()
+//        obj.backgroundColor = .systemBlue
+//        obj.barTintColor = .lightGray.withAlphaComponent(0.5)
+//        obj.isHidden = true
+//        obj.layer.cornerRadius = 10
+//        return obj
+//    }()
+//    
+//    let showSearch: UIButton = {
+//        let obj = UIButton()
+//        obj.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+//        obj.isHidden = false
+//        return obj
+//    }()
     
     let segmentedControl: UISegmentedControl = {
             let control = UISegmentedControl(items: ["movies", "series"])
@@ -50,9 +70,9 @@ class LeftView: UIView{
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addSubview(showSearch)
         
-        showSearch.addTarget(self, action: #selector(showSearchTapped), for: .touchUpInside)
+        
+//        showSearch.addTarget(self, action: #selector(showSearchTapped), for: .touchUpInside)
         
         LeftSetup()
     }
@@ -61,16 +81,17 @@ class LeftView: UIView{
         fatalError("init(coder:) has not been implemented")
     }
     
-    @objc func showSearchTapped() {
-        search.isHidden = false
-        showSearch.isHidden = true
-        search.becomeFirstResponder()
-    }
+//    @objc func showSearchTapped() {
+//        search.isHidden = false
+//        showSearch.isHidden = true
+//        search.becomeFirstResponder()
+//    }
     
     private func addSubviews(){
         self.addSubview(containerView)
-        containerView.addSubview(search)
-        containerView.addSubview(showSearch)
+//        containerView.addSubview(search)
+//        containerView.addSubview(showSearch)
+        containerView.addSubview(searchTextField)
         containerView.addSubview(segmentedControl)
         containerView.addSubview(collectionView)
     }
@@ -81,19 +102,27 @@ class LeftView: UIView{
             make.edges.equalToSuperview()
         }
         
-        search.snp.makeConstraints() { make in
+//        search.snp.makeConstraints() { make in
+//            make.height.equalTo(30)
+//            make.width.equalTo(300)
+//            make.centerX.equalToSuperview().offset(0)
+//            make.centerY.equalToSuperview().offset(-350)
+//            make.leading.trailing.equalToSuperview()
+//        }
+//        
+//        showSearch.snp.makeConstraints { make in
+//            make.height.equalTo(30)
+//            make.width.equalTo(20)
+//            make.centerX.equalToSuperview().offset(-170)
+//            make.centerY.equalToSuperview().offset(-350)
+//        }
+        
+        searchTextField.snp.makeConstraints { make in
             make.height.equalTo(30)
             make.width.equalTo(300)
             make.centerX.equalToSuperview().offset(0)
             make.centerY.equalToSuperview().offset(-350)
             make.leading.trailing.equalToSuperview()
-        }
-        
-        showSearch.snp.makeConstraints { make in
-            make.height.equalTo(30)
-            make.width.equalTo(20)
-            make.centerX.equalToSuperview().offset(-170)
-            make.centerY.equalToSuperview().offset(-350)
         }
         
         segmentedControl.snp.makeConstraints { make in
